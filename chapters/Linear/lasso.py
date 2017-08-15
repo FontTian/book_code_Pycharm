e37f7d0bdb60ee98a7288fc9ab0136c2bee95189
田丰 <fonttian@163.com>
2017-08-15 16:39:13 +0800

    更新版本至:sklearn_0.19.0
    1. 更新版本至0.19.0,提前修改了将要在0.20中remove的类包
    2. 修改了code中的方法名,避免被pycharm识别为 单元测试 
    3. 修改了code中的'diabetes'误写为'datasets'的错误

--- chapters/Linear/lasso.py
+++ chapters/Linear/lasso.py
@@ -10,7 +10,9 @@
 """
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import datasets, linear_model,cross_validation
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+
 def load_data():
     '''
     加载用于回归问题的数据集
@@ -18,9 +20,9 @@ def load_data():
     :return: 一个元组，用于回归问题。元组元素依次为：训练样本集、测试样本集、训练样本集对应的值、测试样本集对应的值
     '''
     diabetes = datasets.load_diabetes()#使用 scikit-learn 自带的一个糖尿病病人的数据集
-    return cross_validation.train_test_split(datasets.data,diabetes.target,
+    return train_test_split(diabetes.data,diabetes.target,
 		test_size=0.25,random_state=0) # 拆分成训练集和测试集，测试集大小为原始数据集大小的 1/4
-def test_Lasso(*data):
+def Lasso(*data):
     '''
     测试 Lasso 的用法
 
@@ -33,7 +35,7 @@ def test_Lasso(*data):
     print('Coefficients:%s, intercept %.2f'%(regr.coef_,regr.intercept_))
     print("Residual sum of squares: %.2f"% np.mean((regr.predict(X_test) - y_test) ** 2))
     print('Score: %.2f' % regr.score(X_test, y_test))
-def test_Lasso_alpha(*data):
+def Lasso_alpha(*data):
     '''
     测试 Lasso 的预测性能随 alpha 参数的影响
 
@@ -58,5 +60,5 @@ def test_Lasso_alpha(*data):
     plt.show()
 if __name__=='__main__':
     X_train,X_test,y_train,y_test=load_data() # 产生用于回归问题的数据集
-    test_Lasso(X_train,X_test,y_train,y_test) # 调用 test_Lasso
-    # test_Lasso_alpha(X_train,X_test,y_train,y_test) # 调用 test_Lasso_alpha
+    Lasso(X_train,X_test,y_train,y_test) # 调用 Lasso
+    Lasso_alpha(X_train,X_test,y_train,y_test) # 调用 Lasso_alpha
